b10a6ab2732a6c81736e4407259c8f7a44fe1a07
andreoliveiraalves96 <andreoliveiraalves96@gmail.com>
2025-11-07 16:31:27 +0000

    fixed cors issues

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -62,12 +62,10 @@ export const loginUser = async (req, res) => {
         const token = generateToken(existingUser._id)
 
         // Set token as HttpOnly cookie
-        const isProduction = process.env.NODE_ENV === 'production'
-
         res.cookie('token', token, {
             httpOnly: true,
-            secure: isProduction,
-            sameSite: isProduction ? 'None' : 'Lax',
+            secure: false,       
+            sameSite: 'None',    
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
